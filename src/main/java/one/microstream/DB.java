5698269f720292d21b6ce02a97081171e32d757d
Kümmel Christian <c.kuemmel@microstream.one>
2021-09-10 12:29:31 +0200

    Zwischencommit

---
+++ src/main/java/one/microstream/DB.java
@@ -0,0 +1,46 @@
+package one.microstream;
+
+import java.net.URL;
+import java.util.Optional;
+
+import io.micronaut.core.io.ResourceResolver;
+import io.micronaut.core.io.scan.ClassPathResourceLoader;
+import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
+import one.microstream.storage.embedded.types.EmbeddedStorageManager;
+
+
+public class DB
+{
+	public static EmbeddedStorageManager	storageManager_1;
+	public final static DataRoot			root_1	= new DataRoot();
+	public static EmbeddedStorageManager	storageManager_2;
+	public final static DataRoot			root_2	= new DataRoot();
+	public static EmbeddedStorageManager	storageManager_3;
+	public final static DataRoot			root_3	= new DataRoot();
+	
+	public static void initializeXMLStorage()
+	{
+		ClassPathResourceLoader loader = new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
+		Optional<URL> resource = loader.getResource("microstream.xml");
+		
+		storageManager_1 = EmbeddedStorageConfiguration.load(
+			resource.get().getPath()).createEmbeddedStorageFoundation().createEmbeddedStorageManager(root_1).start();
+	}
+	
+	public static void initializeBuilderStorage()
+	{
+		
+	}
+	
+	public static void initializeFoundationStorage()
+	{
+		
+	}
+	
+	static
+	{
+		initializeXMLStorage();
+		initializeBuilderStorage();
+		initializeFoundationStorage();
+	}
+}
---
+++ src/main/java/one/microstream/DataRoot.java
@@ -0,0 +1,6 @@
+package one.microstream;
+
+public class DataRoot
+{
+	
+}
