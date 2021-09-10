d505236e82863327459220d567dad72ec942a5da
Kümmel Christian <c.kuemmel@microstream.one>
2021-09-10 11:51:28 +0200

    Zwischencommit

--- src/main/java/one/microstream/Application.java
+++ src/main/java/one/microstream/Application.java
@@ -2,9 +2,13 @@ package one.microstream;
 
 import io.micronaut.runtime.Micronaut;
 
-public class Application {
 
-    public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
-    }
+public class Application
+{
+	
+	public static void main(String[] args)
+	{
+		Micronaut.run(Application.class, args);
+		
+	}
 }
