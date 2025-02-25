31c24b6b8a1488ae36879a8634b6156393a3f5ae
Kümmel Christian <c.kuemmel@microstream.one>
2025-02-25 12:14:50 +0100

    Upgrade to EclipseStore 2.1.2, Remove static content

--- src/main/java/one/microstream/DB.java
+++ src/main/java/one/microstream/DB.java
@@ -1,47 +1,37 @@
 package one.microstream;
 
-import java.net.URL;
-import java.util.Optional;
-
-import org.eclipse.store.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
 import org.eclipse.store.storage.embedded.types.EmbeddedStorageManager;
 
-import io.micronaut.core.io.ResourceResolver;
-import io.micronaut.core.io.scan.ClassPathResourceLoader;
+import io.micronaut.context.annotation.Value;
+import jakarta.inject.Singleton;
 
 
+@Singleton
 public class DB
 {
-	public static EmbeddedStorageManager	storageManager_1;
-	public final static DataRoot			root_1	= new DataRoot();
-	public static EmbeddedStorageManager	storageManager_2;
-	public final static DataRoot			root_2	= new DataRoot();
-	public static EmbeddedStorageManager	storageManager_3;
-	public final static DataRoot			root_3	= new DataRoot();
+	@Value("${storage.path}") private String	path;
 	
-	public static void initializeXMLStorage()
-	{
-		ClassPathResourceLoader loader = new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
-		Optional<URL> resource = loader.getResource("microstream.xml");
-		
-		storageManager_1 = EmbeddedStorageConfiguration.load(
-			resource.get().getPath()).createEmbeddedStorageFoundation().createEmbeddedStorageManager(root_1).start();
-	}
+	public EmbeddedStorageManager				storageManager_1;
+	public final DataRoot						root_1	= new DataRoot();
+	public EmbeddedStorageManager				storageManager_2;
+	public final DataRoot						root_2	= new DataRoot();
+	public EmbeddedStorageManager				storageManager_3;
+	public final DataRoot						root_3	= new DataRoot();
 	
-	public static void initializeBuilderStorage()
+	public void initializeXMLStorage()
 	{
 		
 	}
 	
-	public static void initializeFoundationStorage()
+	public void initializeBuilderStorage()
 	{
+		// @formatter:off
 		
+		// @formatter:on 
 	}
 	
-	static
+	public void initializeFoundationStorage()
 	{
-		initializeXMLStorage();
-		initializeBuilderStorage();
-		initializeFoundationStorage();
+		
 	}
 }
